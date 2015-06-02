ebf9f2de6a86d3760b1bdadfa035b3eb3f3f6700
Evgeny Demidchenko <promocoding@gmail.com>
2015-06-02 23:02:58 +0300

    Register Socialize fasade

--- server/www/config/app.php
+++ server/www/config/app.php
@@ -192,6 +192,7 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+        'Socialize' => 'Laravel\Socialite\Facades\Socialite',
 
 	],
 
