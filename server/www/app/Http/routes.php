b4df06093a110be0b3b2415bbedd44cb16771f12
nik6dev <nik6dev@gmail.com>
2015-06-04 13:34:13 +0300

    Configured mail queue.

--- server/www/app/Http/routes.php
+++ server/www/app/Http/routes.php
@@ -13,3 +13,14 @@ Route::controllers([
 /*Route::group(array('prefix' => 'v1'), function () {
     Route::resource('dj', 'DjController', ['except' => ['create', 'edit']]);
 });*/
+
+//This is a test mail queue route
+Route::get('/qmail', function () {
+    Mail::queue('emails.password', ['token' => 'token_value'], function ($message) {
+        $message->from('lr@example.com', 'Laravel');
+        $message->to('foo@example.com', 'John Smith')->subject('Welcome!');
+    });
+    return 'QUEUED ' . time()
+    . '</br>Run queued jobs by: php artisan queue:listen --timeout=2 --tries=3'
+    . '</br> also take a look at http://laravel.com/docs/5.0/artisan#scheduling-artisan-commands ';
+});
